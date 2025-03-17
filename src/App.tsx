332e39e099d0976c939a21d5f31203107b8542e9
Ashish Kumar <ashparmar08@gmail.com>
2025-03-17 20:42:52 +0530

    l

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,9 @@ import { motion } from "framer-motion";
 import CoursesPage from "./components/CoursesPage";
 import NewsSection from "./components/NewsSection";
 import ScrollToTop from "./ScrollToTop";
+import logo from "../logo.png";
+
+
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
